f80f3cc907a410adb821bbafd5b4d85ec28e3bb5
CestLou <alisonrebeccarobb@gmail.com>
2015-03-02 22:43:31 -0800

    cleaned up code

--- js/template.js
+++ js/template.js
@@ -1,12 +1,3 @@
-/* Theme Name: Worthy - Free Powerful Theme by HtmlCoder
- * Author:HtmlCoder
- * Author URI:http://www.htmlcoder.me
- * Version:1.0.0
- * Created:November 2014
- * License: Creative Commons Attribution 3.0 License (https://creativecommons.org/licenses/by/3.0/)
- * File Description: Initializations of plugins
- */
-
 (function($){
 	$(document).ready(function(){
 
